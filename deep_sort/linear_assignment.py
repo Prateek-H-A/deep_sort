be4630de3962fd9123a2d45bacefa6b70c9c794d
Prateek-H-A <118420110+Prateek-H-A@users.noreply.github.com>
2024-07-07 19:21:31 +0530

    Update linear_assignment.py

--- deep_sort/linear_assignment.py
+++ deep_sort/linear_assignment.py
@@ -1,6 +1,3 @@
-Updates to keyboard shortcuts … 
-On Thursday, 1 August 2024, Drive keyboard shortcuts will be updated to give you first-letter navigation.Learn more
-
 # vim: expandtab:ts=4:sw=4
 from __future__ import absolute_import
 import numpy as np
